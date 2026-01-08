ecea77dd4f4f21557525b4cf1d0e03a0ea830b8c
orbarak808 <orbarak808@gmail.com>
2026-01-08 16:05:02 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -61,9 +61,6 @@ export default function Home() {
 
       <ProgramTimeline />
 
-      <AnimatedDivBreathing>
-       {/* <HighlightsSection /> */}
-      </AnimatedDivBreathing>
 
       <SafetySection />
 
